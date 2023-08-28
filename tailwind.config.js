a29c01e9ef7bdfe134c28cca4031a3125669a971
Almas Ramazanov <ramazanov_almas@list.ru>
2023-08-28 20:41:43 +0600

    Changed the Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   mode: "jit",
   theme: {
     extend: {
-      colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
-      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
@@ -27,5 +18,4 @@ export default {
     },
   },
   plugins: [],
-}
-
+};
